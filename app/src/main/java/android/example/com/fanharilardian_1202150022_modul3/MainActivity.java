448355ec9c32463c76b91056435f108f73f34319
fanharilardian <fanharilfanharil@gmail.com>
2018-02-24 00:30:17 +0700

    Commit Pertama Kali

---
+++ app/src/main/java/android/example/com/fanharilardian_1202150022_modul3/MainActivity.java
@@ -0,0 +1,13 @@
+package android.example.com.fanharilardian_1202150022_modul3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
