fda6716c2112c78b4e034e72e952a2298db913db
fanharilardian <fanharilfanharil@gmail.com>
2018-02-25 20:56:22 +0700

    Commit Keempat - Complete!

--- app/src/main/java/android/example/com/fanharilardian_1202150022_modul3/DetailMenu.java
+++ app/src/main/java/android/example/com/fanharilardian_1202150022_modul3/DetailMenu.java
@@ -3,8 +3,10 @@ package android.example.com.fanharilardian_1202150022_modul3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Fanharil Ardian on 24/02/2018.
@@ -12,8 +14,10 @@ import android.widget.TextView;
 
 public class DetailMenu extends AppCompatActivity {
 
-    private TextView NamaAirMineral,HargaAirMineral,DeskripsiAirMineral;
+    private TextView NamaAirMineral,HargaAirMineral,DeskripsiAirMineral, liter;
     private ImageView GambarAirMineral;
+    private int liters = 0;
+    ImageView gambarBatere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,9 @@ public class DetailMenu extends AppCompatActivity {
         HargaAirMineral = (TextView) findViewById(R.id.TVHargaMakanan);
         DeskripsiAirMineral = (TextView) findViewById(R.id.deskripsiAirMineral);
         GambarAirMineral = (ImageView) findViewById(R.id.imageViewAirMineral);
+
+        liter = (TextView)findViewById(R.id.Liter);
+        gambarBatere = (ImageView)findViewById(R.id.gallon);
 
         Intent ambilintentdarimyadapter = getIntent();
 
@@ -37,8 +44,46 @@ public class DetailMenu extends AppCompatActivity {
         HargaAirMineral.setText(hrga);
         DeskripsiAirMineral.setText(kmposisi);
         GambarAirMineral.setImageResource(gbr);
+        GALON();
 
+    }
 
+    public void isiGalon(View view) {
+        liters++;
+        GALON();
+    }
+
+    public void kosonginGalon(View view) {
+        liters--;
+        GALON();
+    }
+
+    public void GALON(){
+        if (liters <= 0 ){
+            liter.setText("2L");
+            gambarBatere.setImageResource(R.drawable.ic_gallon1);
+            Toast.makeText(this,"Galon terisi sedikit!",Toast.LENGTH_SHORT).show();
+        }else if (liters == 1){
+            liter.setText("5L");
+            gambarBatere.setImageResource(R.drawable.ic_gallon2);
+        }else if(liters == 2){
+            liter.setText("7L");
+            gambarBatere.setImageResource(R.drawable.ic_gallon3);
+        }else if(liters == 3){
+            liter.setText("9L");
+            gambarBatere.setImageResource(R.drawable.ic_gallon4);
+        }else if(liters == 4){
+            liter.setText("11L");
+            gambarBatere.setImageResource(R.drawable.ic_gallon5);
+        }else if(liters == 5){
+            liter.setText("15L");
+            gambarBatere.setImageResource(R.drawable.ic_gallon6);
+        }else{
+            // jika galon mencapai penuh
+            liter.setText("19L");
+            gambarBatere.setImageResource(R.drawable.ic_gallon7);
+            Toast.makeText(this,"Galon sudah penuh!",Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
